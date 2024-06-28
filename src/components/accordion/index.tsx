b0dc911400b6e46702044a474fd749344ef94c74
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-06-28 20:23:52 +0900

    ✨ Feat: 아코디언 바닐라 ver4

--- src/components/accordion/index.tsx
+++ src/components/accordion/index.tsx
@@ -2,6 +2,7 @@ import cx from "./cx";
 import Accordion1 from "./1_react";
 import Accordion2 from "./2_react";
 import Accordion3 from "./3_react";
+import Accordion4 from "./4_vanilla";
 
 const Accordions = () => {
   return (
@@ -10,6 +11,7 @@ const Accordions = () => {
       <Accordion1 />
       <Accordion2 />
       <Accordion3/>
+      <Accordion4/>
     </div>
   );
 };
