bd8d5a0998ba6d26e3df5f7c2bdb9bc62ccba471
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-06-28 20:53:34 +0900

    ⚠️ Wip: ver 6,7,8

--- src/components/accordion/index.tsx
+++ src/components/accordion/index.tsx
@@ -3,6 +3,10 @@ import Accordion1 from "./1_react";
 import Accordion2 from "./2_react";
 import Accordion3 from "./3_react";
 import Accordion4 from "./4_vanilla";
+import Accordion5 from "./5_vanilla";
+import Accordion6 from "./6_react";
+import Accordion7 from "./7_react";
+import Accordion8 from "./8_react";
 
 const Accordions = () => {
   return (
@@ -10,8 +14,12 @@ const Accordions = () => {
       <h2>아코디언</h2>
       <Accordion1 />
       <Accordion2 />
-      <Accordion3/>
-      <Accordion4/>
+      <Accordion3 />
+      <Accordion4 />
+      <Accordion5 />
+      <Accordion6 />
+      <Accordion7 />
+      <Accordion8 />
     </div>
   );
 };
