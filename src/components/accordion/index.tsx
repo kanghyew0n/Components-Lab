196792aa331f091059e4816b04387eb9e87f0862
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-06-28 19:40:49 +0900

    ✨ Feat: 아코디언 ver3 작업

--- src/components/accordion/index.tsx
+++ src/components/accordion/index.tsx
@@ -1,6 +1,7 @@
 import cx from "./cx";
 import Accordion1 from "./1_react";
 import Accordion2 from "./2_react";
+import Accordion3 from "./3_react";
 
 const Accordions = () => {
   return (
@@ -8,6 +9,7 @@ const Accordions = () => {
       <h2>아코디언</h2>
       <Accordion1 />
       <Accordion2 />
+      <Accordion3/>
     </div>
   );
 };
